4fa687b2dffa61c1b35f897f9de5ac50230cc900
Rustam I <rustam.ibragiomv@fors.ru>
2024-04-08 22:32:17 +0300

    2st task sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  let newArr = [...arr];
 
+  newArr.sort((a, b) => a.localeCompare(b, ["ru", "en"], { caseFirst: 'upper' }));
+
+  if (param === 'asc') {
+    return newArr;
+  } else {
+    return newArr.reverse();
+  }
 }
